48f1f84602d9965ccd48675e62e8119243358fe5
Montana Williams <montywill1025@gmail.com>
2026-06-09 14:43:41 +0000

    feat: add VPC module with subnets, IGW, NAT gateway, and route tables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,41 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for private subnet 1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for private subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+variable "project_name" {
+  description = "Project name for tagging"
+  type        = string
+  default     = "finflow"
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
