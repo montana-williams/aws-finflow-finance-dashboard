af3f800aa61345cfa3725c1e8a9bb32223e7a911
Montana Williams <montywill1025@gmail.com>
2026-06-12 16:17:29 +0000

    feat: add IAM module, fix serverless and database outputs for root module wiring

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -12,3 +12,8 @@ output "db_replica_endpoint" {
   description = "The connection endpoint for the read replica"
   value       = aws_db_instance.replica.endpoint
 }
+
+output "db_instance_identifier" {
+  description = "RDS instance identifier for CloudWatch alarms"
+  value       = aws_db_instance.main.identifier
+}
--- modules/serverless/outputs.tf
+++ modules/serverless/outputs.tf
@@ -27,3 +27,28 @@ output "end_of_month_rule_arn" {
   description = "ARN of the end of month EventBridge rule"
   value       = aws_cloudwatch_event_rule.end_of_month.arn
 }
+
+output "lambda_dashboard_name" {
+  description = "Name of the dashboard Lambda function"
+  value       = aws_lambda_function.lambda_dashboard.function_name
+}
+
+output "lambda_hourly_sync_name" {
+  description = "Name of the hourly sync Lambda function"
+  value       = aws_lambda_function.lambda_hourly_sync.function_name
+}
+
+output "lambda_reporting_name" {
+  description = "Name of the reporting Lambda function"
+  value       = aws_lambda_function.lambda_reporting.function_name
+}
+
+output "dashboard_dlq_name" {
+  description = "Name of the dashboard dead letter queue"
+  value       = aws_sqs_queue.dashboard_dlq.name
+}
+
+output "reporting_dlq_name" {
+  description = "Name of the reporting dead letter queue"
+  value       = aws_sqs_queue.reporting_dlq.name
+}
